7e262da6e6730d7161f8c4b19178a7a29408caf9
hubert-kniola <hubert.kniola@gmail.com>
2021-03-14 18:51:43 +0100

    redefined project structure & adding frontend directory

--- project/etesty/manage.py
+++ project/etesty/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mySite.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'etesty.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
