952bbf4f2c212bf264b05c268d507ffe8413e9e7
bart-kosmala <bartoshmen@gmail.com>
2021-03-16 15:19:37 +0100

    intro redux

--- etesty/webapp/src/index.js
+++ etesty/webapp/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import { rootReducer } from './reducers';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ etesty/webapp/src/reducers/index.js
@@ -0,0 +1,7 @@
+const initState = {
+
+}
+
+export const rootReducer = (state = initState, action) => {
+    return state;
+}
