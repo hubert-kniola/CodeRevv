2065b9e3eab6915e4236891be98f9e247a6d98ea
Jordan Kondracki <jordankondracki1999@gmail.com>
2021-03-18 12:58:25 +0100

    init db update

--- etesty/server/admin.py
+++ etesty/server/admin.py
@@ -3,10 +3,5 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(ReportTeacher)
-admin.site.register(ReportStudent)
-admin.site.register(Student)
-admin.site.register(StudentGroup)
 admin.site.register(OnlineTest)
-admin.site.register(Teacher)
+admin.site.register(User)
--- etesty/server/models.py
+++ etesty/server/models.py
@@ -2,57 +2,41 @@ from djongo import models
 
 # Create your models here.
 
+
+class User(models.Model):
+    first_name = models.TextField(max_length=20)
+    last_name = models.TextField(max_length=20)
+    email = models.TextField()
+    birth_date = models.DateField(null=True)
+
+
 class Question(models.Model):
-    question_type = models.BooleanField()
+    question_type = models.TextField()
     content = models.TextField()
     answer = models.TextField()
+    max_score = models.FloatField(null=True)
+    image = models.ImageField(null=True)
 
     def __str__(self):
         return self.content
 
 
-class ReportTeacher(models.Model):
-    score = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.score
-
-
-class ReportStudent(models.Model):
+class UserAnswer(models.Model):
+    question_id = models.IntegerField()
+    answer = models.TextField()
+    user = User
+    comment = models.TextField()
     score = models.FloatField()
-
-    def __str__(self):
-        return self.score
-
-
-class Student(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    birth_date = models.DateField(max_length=10)
-
-    def __str__(self):
-        return '%s %s' % (self.first_name, self.last_name)
-
-
-class StudentGroup(models.Model):
-    students = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
 
 
 class OnlineTest(models.Model):
-    headline = models.CharField(max_length=100)
-    pub_date = models.DateField(max_length=10)
+    headline = models.TextField()
+    pub_test = models.DateField(null=True)
+    creator = User
+    users = models.ManyToManyField(User)
+    question = models.ManyToManyField(Question)
+    user_answer = models.ManyToManyField(UserAnswer)
 
-    def __str__(self):
-        return '%s %s' % (self.headline, self.pub_date)
 
 
-class Teacher(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    birth_date = models.DateField(max_length=10)
-
-    def __str__(self):
-        return '%s %s' % (self.first_name, self.last_name)
 
