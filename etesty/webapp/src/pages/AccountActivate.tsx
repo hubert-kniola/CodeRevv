13cc0f1c7d4d563bd6bb2c533a60ed0abccca4b9
bart-kosmala <bartoshmen@gmail.com>
2021-04-11 10:17:25 +0200

    activate params fix

--- etesty/webapp/src/pages/AccountActivate.tsx
+++ etesty/webapp/src/pages/AccountActivate.tsx
@@ -3,26 +3,33 @@ import { FunctionComponent, useEffect, useState } from 'react';
 import { HomeNav, HomeFooter } from 'containers';
 import { LoadingOverlay, Title } from 'components';
 import { apiAxios } from 'utility';
+import { useParams } from 'react-router-dom';
 
-type Props = {
+type RouteParams = {
   uid: string;
   token: string;
 };
 
-export const AccountActivate: FunctionComponent<Props> = ({ uid, token }) => {
+export const AccountActivate: FunctionComponent = () => {
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState<string | null>(null);
+  const { uid, token } = useParams<RouteParams>();
 
   useEffect(() => {
     setLoading(true);
 
     const notifyApi = async () => {
-      const { data } = await apiAxios.post('/activate/', { uid, token });
-      setMessage(
-        data.success === false
-          ? 'Link aktywacji konta jest niepoprawny.'
-          : 'Dziękujemy za założenie konta. Możesz się teraz zalogować.'
-      );
+      try {
+        const { data } = await apiAxios.post('/activate/', { uid, token });
+        setMessage(
+          data.success === true
+            ? 'Dziękujemy za założenie konta. Możesz się teraz zalogować.'
+            : 'Link aktywacji konta jest niepoprawny.'
+        );
+      } catch (err) {
+        setMessage('Wystąpił problem z połączeniem. Spróbuj odświeżyć przeglądarkę.');
+      }
+
       setLoading(false);
     };
 
