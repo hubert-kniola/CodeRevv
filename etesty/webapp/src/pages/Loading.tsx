780a70679049a95848d16db00260d3b5b70b21c8
bart-kosmala <bartoshmen@gmail.com>
2021-04-12 21:24:24 +0200

    no navbar

--- etesty/webapp/src/pages/Loading.tsx
+++ etesty/webapp/src/pages/Loading.tsx
@@ -1,16 +1,11 @@
 import { FunctionComponent } from 'react';
 
-import { HomeFooter, HomeNav } from 'containers';
 import { LoadingOverlay } from 'components';
 
 export const Loading: FunctionComponent = () => (
   <>
-    <HomeNav />
-
     <LoadingOverlay active={true} text="Jeszcze moment...">
       <div style={{ height: '300px' }} />
     </LoadingOverlay>
-
-    <HomeFooter />
   </>
 );
