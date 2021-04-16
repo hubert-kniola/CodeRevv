8febf1187407182dcd5ccd1f276e9cf95bf0a05d
bart-kosmala <bartoshmen@gmail.com>
2021-04-16 21:51:27 +0200

    init

--- backend/authservice/authservice/wsgi.py
+++ backend/authservice/authservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for etesty project.
+WSGI config for authservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'etesty.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authservice.settings')
 
 application = get_wsgi_application()
