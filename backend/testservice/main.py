0020349092b3849cf2351119d276c20a9f2fbcb5
bart-kosmala <bartoshmen@gmail.com>
2021-04-21 14:34:32 +0200

    working fastapi and mongo

--- backend/testservice/main.py
+++ backend/testservice/main.py
@@ -1,34 +1,40 @@
-from fastapi import FastAPI, Body, status
 import uvicorn
-import motor.motor_asyncio
+
+from fastapi import FastAPI, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from typing import List
+from odmantic import AIOEngine
+from motor.motor_asyncio import AsyncIOMotorClient
 
 import models
 
+MONGODB_URL = 'mongodb+srv://admin:admin@cluster0.k1eh0.mongodb.net/testdb?retryWrites=true&w=majority'
+
 app = FastAPI()
-
-MONGODB_URL='mongodb+srv://admin:<admin>@cluster0.k1eh0.mongodb.net/Cluster0?retryWrites=true&w=majority'
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
-db = client.Cluster0
+client = AsyncIOMotorClient(MONGODB_URL)
+engine = AIOEngine(motor_client=client, database='testdb')
 
 
-@app.post('/create', response_model=models.TestModel)
-async def create_test(test: models.TestModel = Body(...)):
-    test = jsonable_encoder(test)
-    new_test = await db['server_onlinetest'].insert_one(test)
-    created_test = await db['server_onlinetest'].find_one({'_id': new_test.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_test)
+@app.on_event("shutdown")
+def shutdown_event():
+    client.close()
 
 
-@app.get('/tests', response_model=List[models.TestModel])
+@app.post('/create', response_model=models.Test)
+async def create_test(test: models.Test):
+    new_test = await engine.save(test)
+    created_test = await engine.find_one(models.Test, models.Test.id == new_test.id)
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder(created_test))
+
+
+@app.get('/tests', response_model=List[models.Test])
 async def tests_list():
-    tests = await db['server_onlinetest'].find().to_list(1000)
-    return tests
+    tests = await engine.find(models.Test)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(tests))
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
-
-
+    uvicorn.run('main:app', host='0.0.0.0', port=8080, reload=True, debug=True)
--- backend/testservice/models.py
+++ backend/testservice/models.py
@@ -1,79 +1,27 @@
-import uuid
 from typing import Optional, List
 
-from bson import ObjectId
-from pydantic import BaseModel, Field
+from odmantic import Model, EmbeddedModel
 
 
-# class PyObjectId(ObjectId):
-#     @classmethod
-#     def __get_validators__(cls):
-#         yield cls.validate
-#
-#     @classmethod
-#     def validate(cls, v):
-#         if not ObjectId.is_valid(v):
-#             raise ValueError("Invalid objectid")
-#         return ObjectId(v)
-#
-#     @classmethod
-#     def __modify_schema__(cls, field_schema):
-#         field_schema.update(type="string")
+class Question(EmbeddedModel):
+    question_type: str
+    content: str
+    answer: str
+    max_score: float
 
 
-class QuestionModel(BaseModel):
-    question_type: str = Field(...)
-    content: str = Field(...)
-    answer: str = Field(...)
-    max_score: float = Field(...)
+class Answer(EmbeddedModel):
+    question: int
+    answer: str
+    user: int
+    comment: str
+    score: float
 
 
-class AnswerModel(BaseModel):
-    question: int = Field(...)
-    answer: str = Field(...)
-    user: int = Field(...)
-    comment: str = Field(...)
-    score: float = Field(...)
-
-
-class TestModel(BaseModel):
-    name: str = Field(...)
-    pub_test: str = Field(...)
-    creator: int = Field(...)
-    users: list[int] = Field(...)
-    question: Optional[List[QuestionModel]] = Field(...)
-    user_answers: Optional[List[AnswerModel]] = Field(...)
-
-    class Config:
-        schema_extra = {
-            "example": {
-                  "_id": "string",
-                  "name": "string",
-                  "pub_test": "string",
-                  "creator": 0,
-                  "users": [
-                    0
-                  ],
-                  "question": [
-                    {
-                      "_id": "string",
-                      "question_type": "string",
-                      "content": "string",
-                      "answer": "string",
-                      "max_score": 0
-                    }
-                  ],
-                  "user_answers": [
-                    {
-                      "_id": "string",
-                      "question": 0,
-                      "answer": "string",
-                      "user": 0,
-                      "comment": "string",
-                      "score": 0
-                    }
-                  ]
-                }
-        }
-
-
+class Test(Model):
+    name: str
+    pub_test: str
+    creator: int
+    users: List[int]
+    question: Optional[List[Question]]
+    user_answers: Optional[List[Answer]]
