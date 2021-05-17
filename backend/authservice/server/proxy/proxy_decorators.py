ccbbce28d170787418d95020618c38dcbfe45b43
Jordan Kondracki <jordankondracki1999@gmail.com>
2021-05-17 21:50:44 +0200

    fixed proxy decorator

--- backend/authservice/server/proxy/proxy_decorators.py
+++ backend/authservice/server/proxy/proxy_decorators.py
@@ -7,16 +7,21 @@ from rest_framework.exceptions import AuthenticationFailed
 from django.conf import settings
 from rest_framework.exceptions import AuthenticationFailed
 from ..views.serializers import AuthUser
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.exceptions import PermissionDenied
 
 
-def session_authentication(function=None, login_url=None, redirect_field_name=REDIRECT_FIELD_NAME):
+def session_authentication(function, login_url=None, redirect_field_name=REDIRECT_FIELD_NAME):
     def wrap(request):
-        if request.COOKIES['access'] is not None:
+        try:
             token1 = request.COOKIES['access']
             payload = jwt.decode(token1, settings.SECRET_KEY,
                                  settings.SIMPLE_JWT['ALGORITHM'])
             user = AuthUser.objects.get(pk=payload['user_id'])
             if payload['email'] == user.email:
-                return True
-        return False
+                return function(request)
+            raise PermissionDenied
+        except:
+            raise PermissionDenied
     return wrap
--- backend/authservice/server/proxy/proxy_views.py
+++ backend/authservice/server/proxy/proxy_views.py
@@ -23,7 +23,6 @@ proxy = str('http://127.0.0.1:8080')
 @api_view(['POST'])
 @session_authentication
 def test_create(request):
-    print(type(request.data))
     response = requests.post(proxy + '/test/create', json=request.data)
     return Response(response, response.status_code)
 
@@ -36,54 +35,63 @@ def test_list(request):
 
 
 @api_view(['DELETE'])
+@session_authentication
 def test_delete(request):
     response = requests.delete(proxy + '/test/delete?test_id=' + str(request.data['test_id']))
     return Response(response, response.status_code)
 
 
 @api_view(['POST'])
+@session_authentication
 def test_user(request):
     response = requests.post(proxy + '/test/user', json=request.data)
     return Response(response, response.status_code)
 
 
 @api_view(['DELETE'])
+@session_authentication
 def test_user(request):
     response = requests.delete(proxy + '/test/user?test_id=' + str(request.data['test_id']) + '&user_id=' + str(request.data['user_id']))
     return Response(response, response.status_code)
 
 
 @api_view(['POST'])
+@session_authentication
 def test_question(request):
     response = requests.post(proxy + '/test/question?test_id=' + str(request.data['test_id']), json=request.data['data'])
     return Response(response, response.status_code)
 
 
 @api_view(['DELETE'])
+@session_authentication
 def test_question(request):
     response = requests.delete(proxy + '/test/question?test_id=' + str(request.data['test_id']) + '&question_id=' + str(request.data['question_id']))
     return Response(response, response.status_code)
 
 
 @api_view(['PATCH'])
+@session_authentication
 def test_question(request):
     response = requests.patch(proxy + '/test/question?test_id=' + str(request.data['test_id']) + '&question_id=' + str(request.data['question_id']), json=request.data['data'])
     return Response(response, response.status_code)
 
 
 @api_view(['POST'])
+@session_authentication
 def test_answer(request):
     response = requests.post(proxy + '/test/answer?test_id=' + str(request.data['test_id']), json=request.data['data'])
     return Response(response, response.status_code)
 
 
 @api_view(['DELETE'])
+@session_authentication
 def test_answer(request):
     response = requests.delete(proxy + '/test/answer?test_id=' + str(request.data['test_id']) + '&answer_id=' + str(request.data['answer_id']))
     return Response(response, response.status_code)
 
 
 @api_view(['PATCH'])
+@session_authentication
 def test_answer(request):
     response = requests.patch(proxy + '/test/answer?test_id=' + str(request.data['test_id']) + '&answer_id=' + str(request.data['answer_id']), json=request.data['data'])
     return Response(response, response.status_code)
