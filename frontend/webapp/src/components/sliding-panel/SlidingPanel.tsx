c2212be8f25b1eb6309a038b8912f18edaf0adf5
Mateusz Kuźniak <mateokuz@gmail.com>
2021-05-27 17:00:32 +0200

    poprawki

---
+++ frontend/webapp/src/components/sliding-panel/SlidingPanel.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import { Backdrop } from 'components'
+
+type sliderProps={
+    show:boolean;
+}
+
+const SlidingPanel:FC<sliderProps> = ({show}) =>{
+    return(
+        <>
+            test
+        </>
+    )
+}
+
+export default SlidingPanel;
---
+++ frontend/webapp/src/components/sliding-panel/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Panel = styled.div`
+    height: 100%;
+    background-color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 40%;
+    z-index;
+`
