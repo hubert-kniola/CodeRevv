71cc8b374d545e9e76ec27b2ff168a51a0e2b944
bart-kosmala <bartoshmen@gmail.com>
2021-06-09 14:47:10 +0200

    test init

--- frontend/webapp/cypress/integration/login.spec.ts
+++ frontend/webapp/cypress/integration/login.spec.ts
@@ -1,14 +1,33 @@
 /// <reference types="cypress" />
 
-const URL = `http://localhost:3000/signin`;
+const PROXY = `http://127.0.0.1:8000/api/v1`;
 
 describe('Login page', () => {
-    cy.visit(URL);
+  const typedEmail = 'test@mail.com';
+  const typedPassword = 'testpassword';
+  const response = {
+    expiresAt: 10230192039,
+    userInfo: {
+      name: 'Test',
+      surname: 'User',
+      role: 'user',
+    },
+  };
+  const selectors = {
+    email: "input[name='email']",
+    password: "input[name='password']",
+    button: "button[type='submit']",
+  };
 
-    it('fills inputs', () => {
-      
-    });
+  it('fills inputs and logs in', () => {
+    cy.intercept('POST', `${PROXY}/login/`, { statusCode: 200, body: response });
+    cy.visit('/signin');
+
+    cy.get(selectors.email).type(typedEmail).should('have.value', typedEmail);
+    cy.get(selectors.password).type(typedPassword).should('have.value', typedPassword);
+
+    cy.get(selectors.button).click();
   });
-  
-  export {};
-  
+});
+
+export {};
--- frontend/webapp/cypress/integration/mainpage.spec.ts
+++ frontend/webapp/cypress/integration/mainpage.spec.ts
@@ -1,11 +1,8 @@
 /// <reference types="cypress" />
 
-const URL = `http://localhost:3000/`;
-
 describe('Main page', () => {
-  cy.visit(URL);
-  
   it('shows welcome text', () => {
+    cy.visit('/');
     cy.contains('Tu powstaje platforma testów online.');
   });
 });
