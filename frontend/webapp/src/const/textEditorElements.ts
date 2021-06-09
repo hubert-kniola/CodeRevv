ec7f1d7021f23553f91880abd38d3a12c3741a73
bart-kosmala <bartoshmen@gmail.com>
2021-06-09 15:45:29 +0200

    fix text editor

--- frontend/webapp/src/const/textEditorElements.ts
+++ frontend/webapp/src/const/textEditorElements.ts
@@ -1,4 +1,28 @@
-import { ToolbarConfig } from 'react-rte';
+interface StyleConfig {
+  label: string;
+  style: string;
+  className?: string;
+}
+
+type StyleConfigList = StyleConfig[];
+
+type GroupName =
+  | 'INLINE_STYLE_BUTTONS'
+  | 'BLOCK_TYPE_BUTTONS'
+  | 'LINK_BUTTONS'
+  | 'BLOCK_TYPE_DROPDOWN'
+  | 'HISTORY_BUTTONS'
+  | 'IMAGE_BUTTON';
+
+export interface ToolbarConfig {
+  display: GroupName[];
+  extraProps?: object;
+  INLINE_STYLE_BUTTONS: StyleConfigList;
+  BLOCK_TYPE_DROPDOWN: StyleConfigList;
+  BLOCK_TYPE_BUTTONS: StyleConfigList;
+}
+
+// GÓRA DO WYJEBANIA WRAZ Z TYM EDYTOREM
 
 export const toolbarConfig: ToolbarConfig = {
   // Optionally specify the groups to display (displayed in the order listed). IMAGE_BUTTON
@@ -12,11 +36,11 @@ export const toolbarConfig: ToolbarConfig = {
   ],
 
   BLOCK_TYPE_DROPDOWN: [
-    { label: 'Normal', style: 'text-ediotr',},
-    { label: 'Heading Large', style: 'header-one',},
-    { label: 'Heading Medium', style: 'header-two',},
-    { label: 'Heading Small', style: 'header-three',},
-  ], 
+    { label: 'Normal', style: 'text-ediotr' },
+    { label: 'Heading Large', style: 'header-one' },
+    { label: 'Heading Medium', style: 'header-two' },
+    { label: 'Heading Small', style: 'header-three' },
+  ],
 
   BLOCK_TYPE_BUTTONS: [
     { label: 'UL', style: 'unordered-list-item' },
@@ -24,5 +48,3 @@ export const toolbarConfig: ToolbarConfig = {
     { label: 'Blockquote', style: 'blockquote' },
   ],
 };
-
-
