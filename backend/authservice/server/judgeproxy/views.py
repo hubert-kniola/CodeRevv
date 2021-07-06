a8d844fd8381154c658da2a49ca880350ba327ea
bart-kosmala <bartoshmen@gmail.com>
2021-07-06 12:41:56 +0200

    python proxy

--- backend/authservice/server/judgeproxy/views.py
+++ backend/authservice/server/judgeproxy/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from pprint import pprint
 from time import sleep
+from typing import Callable, Dict
 
 from ..models import AuthUser
 from ..utility import get_user_id, make_response_with_cookies, session_authentication
@@ -11,10 +12,31 @@ from ..utility import get_user_id, make_response_with_cookies, session_authentic
 proxy = r'http://3.18.215.227:2358'
 
 
-def force_await_response(callable, pred, interval=0.5, retries=120):
+def force_await_response(callable: Callable[[], None], predicate: Callable[[Dict[str, str]], bool], interval=0.5, retries=120) -> Response:
+    ''' Judge0 wymaga wysłania żądania uruchomienia kodu i dopiero potem pobrania wyniku.
+        Pobranie wyniku nie oznacza że dostaniesz output, status może być '1' czyli 'w kolejce'.
+        Funkcja zapewnia output, lub wyrzuca wyjątek gdy przekroczony zostanie czas.
+        
+        Arguments:
+            * callable -- funkcja wykonująca żądanie
+            * predicate -- funkcja sprawdzająca czy odpowiedź jest ok
+
+        Raises:
+            * TimeoutError -- po przekroczeniu danej ilości prób
+
+        Example: 
+        ```
+        # pobieranie zakończonego przetwarzania kodu z judge0
+        response = force_await_response(
+            callable=lambda _: requests.get(f"{proxy}/submissions/{token}"),
+            predicate=lambda resp: resp['status']['id'] != 1
+        )
+        ```
+
+    '''
     response = callable()
 
-    while not pred(response.json()):
+    while not predicate(response.json()):
         response = callable()
         sleep(interval)
 
@@ -37,10 +59,8 @@ def run_python(request):
     token = response.json()['token']
 
     response = force_await_response(
-        callable=lambda: requests.get(f"{proxy}/submissions/{token}"),
-        pred=lambda resp: resp['status']['id'] != 1
+        callable=lambda _: requests.get(f"{proxy}/submissions/{token}"),
+        predicate=lambda resp: resp['status']['id'] != 1
     )
-
-    pprint(response.json())
 
     return make_response_with_cookies(request, response, response.status_code)
