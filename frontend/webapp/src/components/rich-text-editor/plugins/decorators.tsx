df402500bee5e3401d3fbe50764a5ea6f918c904
Mateusz Kuźniak <mateokuz@gmail.com>
2021-07-31 20:42:09 +0200

    welcome to typescript

--- frontend/webapp/src/components/rich-text-editor/plugins/decorators.tsx
+++ frontend/webapp/src/components/rich-text-editor/plugins/decorators.tsx
@@ -1,10 +1,10 @@
-import { CompositeDecorator } from 'draft-js';
+import { CompositeDecorator, ContentBlock } from 'draft-js';
 import { HashtagSpan } from '../special-block'
 
 const HASHTAG_REGEX = /\#[\w\u0590-\u05ff]+/g;
 const QUOTE_REGEX = /\"[\w\u0590-\u05ff]+"/g;
 
-const findWithRegex = (regex, contentBlock, callback) => {
+const findWithRegex = (regex: RegExp, contentBlock: ContentBlock, callback: (start: number, end: number) => void) => {
   const text = contentBlock.getText();
   let matchArr, start;
   while ((matchArr = regex.exec(text)) !== null) {
@@ -13,11 +13,13 @@ const findWithRegex = (regex, contentBlock, callback) => {
   }
 };
 
-const hashtagStrategy = (contentBlock, callback) => {
+
+
+const hashtagStrategy = (contentBlock: ContentBlock, callback: (start: number, end: number) => void) => {
   findWithRegex(HASHTAG_REGEX, contentBlock, callback);
 };
 
-const quoteStrategy = (contentBlock, callback) => {
+const quoteStrategy = (contentBlock: ContentBlock, callback: (start: number, end: number) => void) => {
   findWithRegex(QUOTE_REGEX, contentBlock, callback);
 };
 
